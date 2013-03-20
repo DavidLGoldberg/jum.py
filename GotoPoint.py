78249544af49b4452345a76a2e029438919962c2
David Goldberg <DavidLGoldberg@gmail.com>
2013-03-20 03:07:26 -0400

    Initial commit.  Mostly working.
    - TODO: Make work on buffer in progress.
    - TODO: Performance.
    - TODO: More than 676 (maybe start from viewport).

---
+++ GotoPoint.py
@@ -0,0 +1,15 @@
+import sublime, sublime_plugin
+
+class GotoPointCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit, row, col):
+		# Convert from 1 based to a 0 based line number
+		row = int(row) - 1
+		col = int(col) - 1
+
+		pt = self.view.text_point(row, col)
+
+		self.view.sel().clear()
+		self.view.sel().add(sublime.Region(pt))
+
+		self.view.show(pt)
---
+++ jum.py
@@ -0,0 +1,97 @@
+import sublime, sublime_plugin
+import GotoPoint
+import tempfile
+import re
+
+class JumpyCommand(sublime_plugin.WindowCommand):
+
+	_keys = list()
+	_locations = list()
+	_bound_keys = dict()
+
+	def create_keys(self):
+		keys = list()
+		for c1 in range(97,123):
+		    for c2 in range(97,123):
+		        keys.append(chr(c1) + chr(c2))
+		self._keys = keys
+
+	def get_all_contents(self, view):
+		region = sublime.Region(0, view.size())
+		return view.substr(region)
+
+	def set_all_word_locations(self):
+		line_num = 0
+		p = re.compile('[a-zA-Z0-9_]{2,}')
+		for line in self._file_contents.splitlines(False):
+			for m in p.finditer(line):
+				self._locations.append((line_num, m.start()))
+			line_num += 1
+
+	def bind_key_locations(self):
+		#This operation handles shortages of keys and or locations for a max of 26 x 26 = 676 keys.
+		self._bound_keys = dict(zip(self._keys, self._locations))
+
+	def store_current_pos(self):
+		self._current_pos = self.window.active_view().rowcol(view.sel()[0].begin())
+
+	def show_bound_keys(self):
+		old_view = self.window.active_view()
+		self._old_viewport = old_view.viewport_position()
+
+		temporary_file = tempfile.NamedTemporaryFile('r')
+		temporary_file.name = "Jumpy to ?"
+		self.window.open_file(temporary_file.name, sublime.TRANSIENT)
+
+		sublime.set_timeout(self.on_labels, 25) #improve this later to check when ready.
+
+	def on_labels(self):
+		new_view = self.window.active_view()
+		new_view.set_scratch(True)
+
+		edit = new_view.begin_edit()
+		new_view.insert(edit, new_view.size(), self._file_contents)
+		new_view.end_edit(edit)
+
+		new_view.set_viewport_position(self._old_viewport)
+
+		regions = list()
+		for k, v in self._bound_keys.items():
+			row, col = v
+			region_start = new_view.text_point(row, col)
+			region_finish = new_view.text_point(row, col + 2)
+
+			region = sublime.Region(region_start, region_finish)
+
+			edit = new_view.begin_edit()
+			new_view.replace(edit, region, k)
+			new_view.end_edit(edit)
+
+			regions.append(region)
+
+		new_view.add_regions('jumpylabel', regions, 'jumpylabel')
+
+	def run(self):
+		self.create_keys()
+
+		view = self.window.active_view()
+		self._file_contents = self.get_all_contents(view)	
+		self.set_all_word_locations()
+
+		self.bind_key_locations()
+
+		self.store_current_pos
+
+		self.show_bound_keys()
+
+		self.window.show_input_panel("Jumpy to ?", "", self.on_key_entered, None, None)
+
+	def on_key_entered(self, key):
+		self._key_entered = key	
+		self.window.run_command('close')			
+
+		original_view = self.window.active_view()
+
+		row, col = self._bound_keys[self._key_entered]
+		original_view.run_command("goto_point", {"row": row + 1, "col": col + 1})
+		print 'jumpy jumped to row: %s, col: %s' % (row, col)
