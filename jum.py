93a40a4c3ec0e63dd7302f7eb0b9a6585c75af1c
David Goldberg <DavidLGoldberg@gmail.com>
2013-03-22 05:49:31 -0400

    Rewrite using Vintage's hotkey architecture.
    - Features: now handles space and escape to cancel out of Jumpy
    - Features: now performs jump on second character (do not need to hit enter
      after entering keys).
    - Readability: pulls out the duplicate_contents steps into a function to
      make more readable.
    - Cleanup: Removes unused get_current_pos method
    - Cleanup: Adds some TODOs
    - Technical: Uses Vintage's architecture which has a static set of
      keybindings that are only applied with the application of certain
      "modes".  This is a built in feature in Sublime.
    - I originally tried by utilizing the on_change and on_cancel of
      show_input_panel.  Ran into issues of closing early on 2nd key press.
      Also had issues with the cancel manipulation as well.  This keeps it
      closer to vim like behavior and Vintage's architecture.

--- jum.py
+++ jum.py
@@ -8,18 +8,18 @@ class JumpyCommand(sublime_plugin.WindowCommand):
 	def create_keys(self):
 		keys = []
 		for c1 in string.lowercase:
-		    for c2 in string.lowercase:
-		        keys.append(c1 + c2)
+			for c2 in string.lowercase:
+				keys.append(c1 + c2)
 		return keys
 
-	def get_file_contents(self, view):
+	def get_file_contents(self, view): #TODO add support for only pulling viewport
 		region = sublime.Region(0, view.size())
 		return view.substr(region)
 
 	def get_all_word_locations(self, file_contents):
 		locations = []
 		line_num = 0
-		p = re.compile('[\w]{2,}')
+		p = re.compile('[\w]{2,}') # TODO: "add something to handle 'quoted symbol characters'
 		for line in file_contents.splitlines(False):
 			for m in p.finditer(line):
 				locations.append((line_num, m.start()))
@@ -30,28 +30,33 @@ class JumpyCommand(sublime_plugin.WindowCommand):
 		#This operation handles shortages of keys and or locations for a max of 26 x 26 = 676 keys.
 		return dict(zip(keys, locations))
 
-	def get_current_pos(self, view):
-		return view.rowcol(view.sel()[0].begin())
-
-	def show_jump_locations(self, old_view):
+	def activate_jumpy_mode(self, old_view):
 		self._old_viewport = old_view.viewport_position()
+
+		global g_key_entered_thus_far
+		g_key_entered_thus_far = ''
 
 		self.window.open_file('Jumpy', sublime.TRANSIENT)
 
 		sublime.set_timeout(self.on_labels, 25) #improve this later to check when ready.
 
 	def on_labels(self):
+		def duplicate_contents(new_view, file_contents):
+			edit = new_view.begin_edit()
+			new_view.insert(edit, new_view.size(), file_contents)
+			new_view.end_edit(edit)	
+		
+		global g_jump_locations
+
 		new_view = self.window.active_view()
 		new_view.set_scratch(True)
 
-		edit = new_view.begin_edit()
-		new_view.insert(edit, new_view.size(), self._file_contents)
-		new_view.end_edit(edit)
+		duplicate_contents(new_view, self._file_contents)
 
 		new_view.set_viewport_position(self._old_viewport)
 
 		regions = []
-		for (key, (row, col)) in self._jump_locations.items():
+		for (key, (row, col)) in g_jump_locations.items():
 			region_start = new_view.text_point(row, col)
 			region_finish = new_view.text_point(row, col + 2)
 
@@ -65,26 +70,56 @@ class JumpyCommand(sublime_plugin.WindowCommand):
 
 		new_view.add_regions('jumpylabel', regions, 'jumpylabel')
 
+		new_view.settings().set('jumpy_jump_mode', True)
+
 	def run(self):
-		keys = self.create_keys()
 		view = self.window.active_view()
-		self._file_contents = self.get_file_contents(view)	
-		locations = self.get_all_word_locations(self._file_contents)
+		if not view.settings().get('jumpy_jump_mode'): # don't open twice
+			global g_jump_locations
 
-		self._jump_locations = self.get_jump_locations(keys, locations)
-		self._current_pos = self.get_current_pos(view)
+			keys = self.create_keys()
+			self._file_contents = self.get_file_contents(view)	
+			locations = self.get_all_word_locations(self._file_contents)
 
-		self.show_jump_locations(view)
+			g_jump_locations = self.get_jump_locations(keys, locations)
 
-		self.window.show_input_panel("Jumpy to ?", "", self.on_key_entered, None, None)
+			self.activate_jumpy_mode(view)
 
-	def on_key_entered(self, key):
-		self._key_entered = key	
-		self.window.run_command('close')			
+class InputKeyPart(sublime_plugin.TextCommand):
+	def clean_up(self):
+		self.view.settings().set('jumpy_jump_mode', False)
+		shortcut_window = self.view.window()
+		shortcut_window.run_command('close')
 
-		original_view = self.window.active_view()
+	def run(self, edit, character):
 
-		row, col = self._jump_locations[self._key_entered]
-		original_view.run_command("goto_point", {"row": row + 1, "col": col + 1})
-		print 'jumpy jumped to row: %s, col: %s' % (row, col)
-		sublime.status_message('jumpy jumped to row: %s, col: %s' % (row, col))
+		def cancel(view):
+			print 'Jumpy: cancel'
+			sublime.status_message('Jumpy: cancel')
+			self.clean_up()
+
+		if character in [' ', 'escape']:
+			cancel(self.view)
+		else:
+			global g_key_entered_thus_far
+			g_key_entered_thus_far = g_key_entered_thus_far + character
+
+			if len(g_key_entered_thus_far) > 1: #Time to jump
+				self.jump(g_key_entered_thus_far)
+			else:
+				sublime.status_message('Jumpy: %s' % g_key_entered_thus_far)
+
+	def jump(self, shortcut_entered):
+		self.clean_up()
+		sublime.set_timeout(self.on_jump_entered, 75)
+
+	def on_jump_entered(self):
+		original_view = sublime.active_window().active_view() # because shortcuts are closed
+
+		row, col = g_jump_locations[g_key_entered_thus_far]
+		row += 1
+		col += 1
+		original_view.run_command("goto_point", {"row": row , "col": col})
+		
+		print 'Jumpy: jumped to row: %s, col: %s' % (row, col)
+		sublime.status_message('Jumpy: jumped to row: %s, col: %s' % (row, col))
