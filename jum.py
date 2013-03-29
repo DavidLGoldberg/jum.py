20f7ac18a391c5d29fe3dbdc87cd7d8c21fa5137
David Goldberg <DavidLGoldberg@gmail.com>
2013-03-28 23:00:12 -0400

    Using class variables instead of globals.
    - first pass at do_when as well.
    - closes #2

--- jum.py
+++ jum.py
@@ -6,6 +6,9 @@ from os.path import basename
 
 class BaseJumpyCommand(sublime_plugin.TextCommand):
 
+	key_entered_thus_far = ''
+	jump_locations = {}
+
 	def set_jumpy_commands(self, new_view, on=True):
 		new_view.settings().set('command_mode', not on)
 		new_view.settings().set('jumpy_jump_mode', on)
@@ -35,12 +38,7 @@ class JumpyCommand(BaseJumpyCommand):
 
 	def activate_jumpy_mode(self):
 		self._old_viewport = self.view.viewport_position()
-		global g_old_offsets
-		g_old_offsets = self.view.rowcol(self.view.layout_to_text(self._old_viewport))
-
-
-		global g_key_entered_thus_far
-		g_key_entered_thus_far = ''
+		BaseJumpyCommand.old_offsets = self.view.rowcol(self.view.layout_to_text(self._old_viewport))
 
 		file_name = 'Jumpy_' + basename(self.view.file_name()) if sublime.load_settings('Jumpy.sublime-settings').get('use_file_extensions') else 'Jumpy'
 		self.view.window().open_file(file_name, sublime.TRANSIENT)
@@ -50,7 +48,7 @@ class JumpyCommand(BaseJumpyCommand):
 	def on_labels(self):
 		def duplicate_contents(new_view, file_contents):
 			edit = new_view.begin_edit()
-			padded_file_contents = file_contents.rjust(len(file_contents) + g_old_offsets[0], '\n')
+			padded_file_contents = file_contents.rjust(len(file_contents) + BaseJumpyCommand.old_offsets[0], '\n')
 			new_view.insert(edit, 0, padded_file_contents)
 			new_view.end_edit(edit)	
 		
@@ -59,11 +57,10 @@ class JumpyCommand(BaseJumpyCommand):
 		new_view.set_scratch(True)
 		duplicate_contents(new_view, self._visible_text)
 
-		global g_jump_locations
 		regions = []
-		for (key, (row, col)) in g_jump_locations.items():
-			row += g_old_offsets[0]
-			col += g_old_offsets[1]
+		for (key, (row, col)) in BaseJumpyCommand.jump_locations.items():
+			row += BaseJumpyCommand.old_offsets[0]
+			col += BaseJumpyCommand.old_offsets[1]
 			region_start = new_view.text_point(row, col)
 			region_finish = new_view.text_point(row, col + 2)
 
@@ -81,14 +78,14 @@ class JumpyCommand(BaseJumpyCommand):
 
 	def run(self, edit):
 		if not self.view.settings().get('jumpy_jump_mode'): # don't open twice
-			global g_jump_locations
-
+			BaseJumpyCommand.key_entered_thus_far = ''
+			
 			keys = self.create_keys()
 
 			self._visible_text = self.view.substr(self.view.visible_region())
 			locations = self.get_all_word_locations(self._visible_text)
 
-			g_jump_locations = self.get_jump_locations(keys, locations)
+			BaseJumpyCommand.jump_locations = self.get_jump_locations(keys, locations)
 
 			self.activate_jumpy_mode()
 
@@ -108,13 +105,12 @@ class InputKeyPart(BaseJumpyCommand):
 		if character in [' ', 'escape']:
 			cancel(self.view)
 		else:
-			global g_key_entered_thus_far
-			g_key_entered_thus_far = g_key_entered_thus_far + character
+			BaseJumpyCommand.key_entered_thus_far = BaseJumpyCommand.key_entered_thus_far + character
 
-			if len(g_key_entered_thus_far) > 1: #Time to jump
-				self.jump(g_key_entered_thus_far)
+			if len(BaseJumpyCommand.key_entered_thus_far) > 1: #Time to jump
+				self.jump(BaseJumpyCommand.key_entered_thus_far)
 			else:
-				sublime.status_message('Jumpy: %s' % g_key_entered_thus_far)
+				sublime.status_message('Jumpy: %s' % BaseJumpyCommand.key_entered_thus_far)
 
 	def jump(self, shortcut_entered):
 		self.clean_up()
@@ -123,12 +119,11 @@ class InputKeyPart(BaseJumpyCommand):
 	def on_jump_entered(self):
 		original_view = sublime.active_window().active_view() # because shortcuts are closed
 
-		row, col = g_jump_locations[g_key_entered_thus_far]
+		row, col = BaseJumpyCommand.jump_locations[BaseJumpyCommand.key_entered_thus_far]
 		row += 1
 		col += 1
 
-		global g_old_offsets
-		row_offset, col_offset = g_old_offsets
+		row_offset, col_offset = BaseJumpyCommand.old_offsets
 		original_view.run_command("goto_point", {"row": row + row_offset, "col": col + col_offset})
 
 		print 'Jumpy: jumped to row: %s, col: %s' % (row, col)
