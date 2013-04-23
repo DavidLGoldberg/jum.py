25e9f561a652ecba29b6f5edeada74b0a7e35b7d
David Goldberg <DavidLGoldberg@gmail.com>
2013-04-23 09:15:16 -0400

    Clean up.

--- jum.py
+++ jum.py
@@ -5,38 +5,34 @@ from base_jumpy import BaseJumpyCommand
 
 class JumpyCommand(BaseJumpyCommand):
 
-	def activate_jumpy_mode(self, views):
-		BaseJumpyCommand.key_entered_thus_far = ''
-		BaseJumpyCommand.indexer.create_index(views)
+	def activate_jumpy_mode(self, view):
+		BaseJumpyCommand.old_offset = view.rowcol(view.layout_to_text(view.viewport_position()))
 
-		for view in views:
-			BaseJumpyCommand.old_offset = view.rowcol(view.layout_to_text(view.viewport_position()))
+		self.set_jumpy_command_mode(view) # must occur before edits for indexing rules
+		view.set_scratch(True) # prevents buffer marked as dirty
 
-			self.set_jumpy_command_mode(view) # must occur before edits for indexing rules
-			view.set_scratch(True) # prevents buffer marked as dirty
+		regions = []
 
-			regions = []
+		edit = view.begin_edit()
+		
+		current_views_labels = BaseJumpyCommand.indexer.all_labels[view]
+		for (key, ((row, col), word)) in current_views_labels:
+			row += BaseJumpyCommand.old_offset[0]
+			col += BaseJumpyCommand.old_offset[1]
+			region_start = view.text_point(row, col)
+			region_finish = view.text_point(row, col + 2)
 
-			edit = view.begin_edit()
-			
-			current_views_labels = BaseJumpyCommand.indexer.all_labels[view]
-			for (key, ((row, col), word)) in current_views_labels:
-				row += BaseJumpyCommand.old_offset[0]
-				col += BaseJumpyCommand.old_offset[1]
-				region_start = view.text_point(row, col)
-				region_finish = view.text_point(row, col + 2)
+			region = sublime.Region(region_start, region_finish)
 
-				region = sublime.Region(region_start, region_finish)
+			view.replace(edit, region, key)
 
-				view.replace(edit, region, key)
+			regions.append(region)
 
-				regions.append(region)
+		view.end_edit(edit)
 
-			view.end_edit(edit)
+		view.add_regions('jumpylabel', regions, 'jumpylabel')
 
-			view.add_regions('jumpylabel', regions, 'jumpylabel')
-
-			view.set_read_only(True) # Disable all non jumpy edits
+		view.set_read_only(True) # Disable all non jumpy edits
 
 	def run(self):
 		windows = sublime.windows()
@@ -50,4 +46,7 @@ class JumpyCommand(BaseJumpyCommand):
 			all_views += window.views()
 
 		print 'Jumpy: jump mode'
-		self.activate_jumpy_mode(all_views)
+		BaseJumpyCommand.key_entered_thus_far = ''
+		BaseJumpyCommand.indexer.create_index(all_views)
+		for view in all_views:
+			self.activate_jumpy_mode(view)
